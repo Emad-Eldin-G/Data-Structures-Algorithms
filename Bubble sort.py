2735c68d04a767051bfddd979a50f9af4095f683
Emad Eldin Gasser <omda.g@outlook.com>
2023-01-03 16:43:49 +0300

    🫧 Bubble Sort Algo

---
+++ Bubble sort.py
@@ -0,0 +1,18 @@
+from random import randrange
+
+
+arr = [[randrange(1, 100) for j in range(10)] for i in range(10)]
+
+def bubble_sort(arr):
+    query_length = len(arr)
+    for j in arr:
+        for i in range(query_length):
+            for z in range(query_length -1):
+                if j[z] > j[z+1]:
+                    j[z], j[z+1] = j[z+1], j[z] 
+    return arr
+            
+print(arr)
+print('\n')
+query = bubble_sort(arr)
+print(query)
