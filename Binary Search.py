1a5f7b5763e425b3ca272a616bf87bdea22e3a6c
Emad Eldin Gasser <omda.g@outlook.com>
2023-01-03 17:42:49 +0300

    🕵🏼 Completed Binary Search Algorithm

--- Binary Search.py
+++ Binary Search.py
@@ -1,17 +1,24 @@
-import random
-
+# The condition to perform a Binary Search
+# is to insure that the list is sorted
 
 def BinarySearch(target, start, end, arr):
     if start > end:
         return 'Not found'
-    
+    # Declaring middle index by finding the avg of the start and end
     middle = (start + end) // 2
-    
+    # Base condition | eventually comes here if target is in arr
     if arr[middle] == target:
         return f'{target} was found at index {middle}'
-        
+
+    elif target > arr[middle]:
+        return BinarySearch(target, middle+1, end, arr)
+
+    elif target < arr[middle]:
+        return BinarySearch(target, start, middle-1, arr)
+
 
 if __name__ == '__main__':
-    array = [random.randrange(1, 50) for i in range(10)]
+    array = [x for x in range(20)]
     lookfor = int(input("Please input a number: "))
-    BinarySearch(lookfor, 0, len(array), array)
+    result = BinarySearch(lookfor, 0, len(array), array)
+    print(result)
