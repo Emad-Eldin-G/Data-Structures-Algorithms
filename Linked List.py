8904dc0688a71c3739cd56961a2b4a2f25dd5ca1
Emad Eldin Gasser <omda.g@outlook.com>
2023-01-05 08:04:51 +0300

    ⛓️Finished Linked List

--- Linked List.py
+++ Linked List.py
@@ -1,6 +1,36 @@
 
 class Node():
-    def __init__(self, data):
-        self.data = data
+    def __init__(self, value):
+        self.value = value
         self.next = None
-        self.previous = None
+        
+class LinkedList():
+    def __init__(self):
+        self.head = None
+
+    def insert(self, value):
+        node = Node(value)
+        if not self.head:
+            self.head = node 
+            return
+        currentNode = self.head
+        while True:
+            if currentNode.next is None:
+                currentNode.next = node
+                break
+            currentNode = currentNode.next
+
+    def output(self):
+        currentNode = self.head
+        while currentNode is not None:
+            print(currentNode.value, end=' -> ')
+            currentNode = currentNode.next
+        print('None')
+
+mylist = LinkedList()
+mylist.insert('3')
+print(mylist.output())
+mylist.insert('5')
+print(mylist.output())
+mylist.insert('8')
+print(mylist.output())
